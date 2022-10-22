75429ff58e434d4cdc5651f9e7a75c3aab95642d
gyandeep <gyandeepsharma007@gmail.com>
2022-10-22 19:27:23 +0530

    base application'

---
+++ src/main/java/com/example/jenkinsspringbootapp/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.jenkinsspringbootapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String greet(){
+        return "welcome to spring boot";
+    }
+}
---
+++ src/test/java/com/example/jenkinsspringbootapp/JenkinsSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
